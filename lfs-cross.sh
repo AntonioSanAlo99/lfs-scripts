b423319e8b3ec138be444ea0760dda69abd5e27b
antoniosanalo99 <111574729+AntonioSanAlo99@users.noreply.github.com>
2025-11-23 12:42:41 +0100

    Fix IO_ftrylockfile usage in m4-1.4.20

--- lfs-cross.sh
+++ lfs-cross.sh
@@ -154,6 +154,8 @@ finish
 
 # 6.2. M4-1.4.20
 begin m4-1.4.20 tar.xz
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
